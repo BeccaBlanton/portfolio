c2379b78ea316c4a5e8d00dd9e00b4eb6871ab6a
Rebecca Blanton <beccaablanton@gmail.com>
2021-03-07 23:32:50 -0800

    added Sticky Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import "./style.css";
+
+function Footer(){
+    return(
+    <footer className="fixed-bottom footer mt-auto py-3">
+        <div className="container">
+            <span>
+                <a href="https://github.com/BeccaBlanton" className="fa fa-github"> </a>
+                <a href="https://www.linkedin.com/in/rebecca-blanton-416028ab/" className="fa fa-linkedin"> </a>
+                <a href="mailto:beccaablanton@gmail.com" className="fa fa-envelope"> </a>
+            </span>
+        </div>
+  </footer>
+    )
+}
+
+export default Footer;
