9e92c3502aad15eb6c19d7c6552ef0eaf5b8078a
Rebecca Blanton <beccaablanton@gmail.com>
2021-03-13 17:47:03 -0800

    moved image files for portfolio, not showing after deployment

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,7 +7,7 @@ function Resume() {
             <div className="row">
                 <div className="col">
                   <a href='https://docs.google.com/document/d/e/2PACX-1vR2OP9YHaCT8ct64Lundv7076lKWFfS_2K5hyEyMEoRt8YYsyTAD4QquD633ixxafh7S4B7bIHZ67tx/pub'>
-                    <img src="../../images/RebeccaBlantonResume.png" className="card-img-top" alt="resume"></img>
+                    <img src="../images/RebeccaBlantonResume.png" className="card-img-top" alt="resume"></img>
                   </a>
                   
                 </div>
